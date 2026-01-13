55b041da6f061ecffcadbffa2bbc9578fb6523a3
danielromeuperez-commits <danielromeuperez@gmail.com>
2026-01-13 12:58:35 +0100

    mis decorations
    pasillo del boss y sala de antes hechas

--- LittleDolores/Assets/_LittleDolores/Scripts/EventManager/GameManager.cs
+++ LittleDolores/Assets/_LittleDolores/Scripts/EventManager/GameManager.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //Declaración del Singleton
-    private static GameManager instance;
-    private static GameManager Instance
+    public static GameManager instance;
+    public static GameManager Instance
     {
         get
         {
@@ -16,9 +16,11 @@ public class GameManager : MonoBehaviour
         //Fin del Singleton
     }
         //DECLARAMOS CUALQUIER VALOR GENERAL EN PUBLIC
-        public int playerHealth;
+        public int playerHealth = 3;
         public int playerPoints;
         public int timeLeft;
+        public int bossMaxHealth = 6;
+        public int bossHealth = 6;
     
         private void Awake()
         {
